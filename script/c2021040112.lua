91b40aa785abffa3d681f396b03d46cb4255823f
Almatik <almatiktk@gmail.com>
2021-06-08 19:51:15 +0600

    Update c2021040112.lua

--- script/c2021040112.lua
+++ script/c2021040112.lua
@@ -7,14 +7,15 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
 	return aux.CanActivateSkill(tp)
 end
-function s.filter(c)
-	return c:IsAbleToRemove() and c:IsType(TYPE_MONSTER)
-end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	--cost
 	local c=e:GetHandler()
-	local g=Duel.GetFieldGroup(tp,LOCATION_GRAVE,0)
-	Duel.SendToDeck(g,nil,-2,REASON_EFFECT)
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_MONSTER)
+	local tc=g:GetFirst()
+	while tc do
+		Duel.SendToDeck(g,nil,-2,REASON_EFFECT)
+		tc=g:GetNext()
+	end
 end
