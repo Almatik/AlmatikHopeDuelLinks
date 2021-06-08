84454b3d1fed8299c0c86b0a6ca29c189493e6c8
Almatik <almatiktk@gmail.com>
2021-06-08 18:57:20 +0600

    Skills

---
+++ script/c2021040109.lua
@@ -0,0 +1,30 @@
+--Three-Star Demotion
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+end
+function s.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return aux.CanActivateSkill(tp) and Duel.CheckLPCost(tp,3000)
+		and Duel.GetFieldGroup(tp,LOCATION_HAND,0):FilterCount(Card.IsLevelAbove,nil,1)>0
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--cost
+	Duel.PayLPCost(tp,3000)
+	local c=e:GetHandler()
+	local hg=Duel.GetFieldGroup(tp,LOCATION_HAND,0):Filter(Card.IsLevelAbove,nil,1)
+	local tc=hg:GetFirst()
+	for tc in aux.Next(hg) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(-3)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
---
+++ script/c2021040110.lua
@@ -0,0 +1,27 @@
+--Harpies' Hunting Ground
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetCondition(s.flipcon)
+	e1:SetOperation(s.flipop)
+	c:RegisterEffect(e1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Field Spell
+	local c=e:GetHandler()
+	local code=CreateToken(tp,75782277)
+	MoveToField(code,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
+end
