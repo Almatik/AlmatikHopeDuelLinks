ba0ac3cf064c1e7713d4a24b7ce8e2c2531c762f
Almatik <almatiktk@gmail.com>
2021-06-08 19:56:43 +0600

    Update c2021040112.lua

--- script/c2021040112.lua
+++ script/c2021040112.lua
@@ -13,5 +13,11 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--cost
 	local c=e:GetHandler()
 	local sdg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND,0,nil,TYPE_MONSTER)
+	local n=sdg:GetCount()
 	Duel.SendtoDeck(sdg,nil,-2,REASON_RULE)
+	repeat 
+		local code=Duel.CreateToken(tp,32274490)
+		Duel.MoveToField(code,tp,tp,LOCATION_GRAVE,POS_FACEUP,true)
+		n=n-1
+	until n=0
 end
