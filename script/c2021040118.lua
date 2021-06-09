bd4f53fd47da639cbc5ef9d7bb909c412a36cad2
Almatik <almatiktk@gmail.com>
2021-06-09 13:12:10 +0600

    Skill!

---
+++ script/c2021040118.lua
@@ -0,0 +1,27 @@
+--Duel Standby (2018)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddPreDrawSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--ask if you want to activate the skill or not
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--look at hand
+	Duel.Draw(tp,1,REASON_EFFECT)
+	Duel.Draw(1-tp,1,REASON_EFFECT)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+end
---
+++ script/c2021040119.lua
@@ -0,0 +1,19 @@
+--Duel Standby (2017)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddPreDrawSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--ask if you want to activate the skill or not
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--look at hand
+	Duel.Draw(tp,1,REASON_EFFECT)
+	Duel.Draw(1-tp,1,REASON_EFFECT)
+end
