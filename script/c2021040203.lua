2c39679600af6c6c79751c66ccc3dcb69c5827cf
Almatik <almatiktk@gmail.com>
2021-06-10 22:03:43 +0600

    Update c2021040203.lua

--- script/c2021040203.lua
+++ script/c2021040203.lua
@@ -3,34 +3,12 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--skill
 	aux.AddSkillProcedure(c,id,false,s.flipcon,s.flipop,1)
-	aux.GlobalCheck(s,function()
-		s[0]=nil
-		s[1]=nil
-		s[2]=0
-		s[3]=0
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_ADJUST)
-		ge1:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge1,0)
-	end)
-end
-function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	if not s[tp] then s[tp]=Duel.GetLP(tp) end
-	if s[tp]>Duel.GetLP(tp) then
-		s[2+tp]=s[2+tp]+(s[tp]-Duel.GetLP(tp))
-		s[tp]=Duel.GetLP(tp)
-	end
-end
-function s.filter(c)
-	return c:IsAbleToDeck()
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--twice per duel check
 	if Duel.GetFlagEffect(ep,id)>1 then return end
 	--condition
-	return aux.CanActivateSkill(tp) and s[2+tp]>=1000
-		and Duel.IsPlayerCanDraw(tp,1)
+	return aux.CanActivateSkill(tp) and Duel.GetLP(tp)<=3000
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
@@ -38,14 +16,11 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,tp,id)
 	--Used skill flag register
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
-	local c=e:GetHandler()
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
-	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
-		Duel.ShuffleDeck(tp)
-		Duel.BreakEffect()
-		Duel.Draw(tp,1,REASON_EFFECT)
-		Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
-		s[2+tp]=0
+	local g=Duel.GetFieldGroup(tp,LOCATION_FZONE,0)
+	if #g>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
 	end
+	local code=Duel.CreateToken(tp,53527835)
+	Duel.MoveToField(code,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
 end
 
