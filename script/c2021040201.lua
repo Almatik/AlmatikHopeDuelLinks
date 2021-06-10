35cc93627dc53cc48afef17a19b82380c34a7143
Almatik <almatiktk@gmail.com>
2021-06-10 16:46:36 +0600

    Add files via upload

---
+++ script/c2021040201.lua
@@ -0,0 +1,18 @@
+--Straight to the Grave
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddPreDrawSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Field Spell
+	local c=e:GetHandler()
+	local code=Duel.CreateToken(tp,23424603)
+	Duel.MoveToField(code,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
+end
