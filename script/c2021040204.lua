b884fc9217fa6c34341e1e90cb5e982eb7ec65a1
Almatik <almatiktk@gmail.com>
2021-06-10 21:41:59 +0600

    Update c2021040204.lua

--- script/c2021040204.lua
+++ script/c2021040204.lua
@@ -11,7 +11,6 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	else
 		local lp=Duel.GetLP(tp)
 	end
-	math.randomseed(os.clock())
 	local n=math.random(4000)
 	--condition
 	return Duel.GetCurrentChain()==0 and tp==Duel.GetTurnPlayer() and lp>n
