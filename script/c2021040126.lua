38d0578aa1fc26ac5f732959437f81021b3a1ef6
Almatik <almatiktk@gmail.com>
2021-06-18 00:23:59 +0600

    Update c2021040126.lua

--- script/c2021040126.lua
+++ script/c2021040126.lua
@@ -7,14 +7,14 @@ function s.initial_effect(c)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
-	return aux.DLCanIgnition(tp)
+	return aux.DLCanIgnition(tp) and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,3,nil,TYPE_SPELL)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	local tc1=Duel.GetFieldGroup(tp,LOCATION_GRAVE,0):Filter(Card.IsType,nil,tp,TYPE_SPELL):RandomSelect(tp,1)
+	local tc1=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL):RandomSelect(tp,1)
 	Duel.SendtoHand(tc1,nil,REASON_EFFECT)
-	local tc2=Duel.GetFieldGroup(tp,LOCATION_GRAVE,0):Filter(Card.IsType,nil,tp,TYPE_SPELL):RandomSelect(tp,1)
+	local tc2=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL):RandomSelect(tp,1)
 	Duel.SendtoDeck(tc2,nil,0,REASON_EFFECT)
 end
