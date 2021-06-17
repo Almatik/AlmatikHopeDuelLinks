1f6065efd6049569622ff2fc2eaf3f3696afe848
Almatik <almatiktk@gmail.com>
2021-06-18 00:42:28 +0600

    Skill

--- script/c2021040129.lua
+++ script/c2021040129.lua
@@ -1,4 +1,4 @@
---Endless Trap Hell
+--The Tie That Binds
 Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
@@ -6,14 +6,10 @@ function s.initial_effect(c)
 	aux.DuelLinksSkill(c,1994,EVENT_FREE_CHAIN,s.flipcon,s.flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--twice per duel check
-	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
-	return aux.DLCanIgnition(tp) and Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_TRAP):GetCount()>=3
+	return aux.DLCanIgnition(tp) and Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetCount()~=0
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	--Used skill flag register
-	Duel.RegisterFlagEffect(ep,id,0,0,0)
 	--place this card to the field
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
