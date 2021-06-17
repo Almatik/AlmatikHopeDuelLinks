41991ca5bbf06b4d1cea4dba8cc73503046bb1a1
Almatik <almatiktk@gmail.com>
2021-06-17 12:32:12 +0600

    Update c2021040124.lua

--- script/c2021040124.lua
+++ script/c2021040124.lua
@@ -28,14 +28,14 @@ end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
 	return s[2+tp]>=0
-		and ep==tp and Duel.GetCurrentPhase(PHASE_DRAW) and Duel.GetTurnPlayer()==tp
+		and ep==tp and Duel.GetCurrentPhase()==PHASE_DRAW and Duel.GetTurnPlayer()==tp
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	local c=e:GetHandler()
-	local code=eg:GetFirst():GetCode()
+	local code=eg:GetCode()
 	local tc=Duel.CreateToken(tp,code)
 	SendtoHand(tc,tp,REASON_EFFECT)
 end
