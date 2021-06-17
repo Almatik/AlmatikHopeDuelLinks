ff19e679e30f15fd4b66d061696593fd9b57f389
Almatik <almatiktk@gmail.com>
2021-06-17 18:34:15 +0600

    Update c2021040124.lua

--- script/c2021040124.lua
+++ script/c2021040124.lua
@@ -2,7 +2,17 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--skill
-	aux.AddVrainsSkillProcedure(c,s.flipcon,s.flipop,EVENT_DRAW)
+	--Activate
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_DRAW)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetCondition(s.flipcon)
+	e1:SetOperation(s.flipop)
+	c:RegisterEffect(e1)
 	aux.GlobalCheck(s,function()
 		s[0]=nil
 		s[1]=nil
