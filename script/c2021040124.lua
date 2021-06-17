f6514944ad9ef32d17bfbc759bfc496291485201
Almatik <almatiktk@gmail.com>
2021-06-17 20:29:59 +0600

    Update c2021040124.lua

--- script/c2021040124.lua
+++ script/c2021040124.lua
@@ -1,17 +1,9 @@
 --Extra Extra
 local s,id=GetID()
 function s.initial_effect(c)
+	c:Cover(1994)
 	--Activate
-	aux.AddSkillProcedure(c,2,false,nil,nil)
-	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_DRAW)
-	e1:SetCountLimit(1)
-	e1:SetRange(0x5f)
-	e1:SetCondition(s.flipcon)
-	e1:SetOperation(s.flipop)
-	c:RegisterEffect(e1)
+	aux.AddVrainsSkillProcedure(c,s.flipcon,s.flipop,EVENT_DRAW)
 	aux.GlobalCheck(s,function()
 		s[0]=nil
 		s[1]=nil
@@ -37,8 +29,6 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.GetTurnPlayer()==tp
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	c:Cover(1994)
 	--place this card to the field
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
