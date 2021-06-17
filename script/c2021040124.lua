8b0a51a25ab45c95c30a010a2aac9fe3c7eee40e
Almatik <almatiktk@gmail.com>
2021-06-17 20:49:35 +0600

    Update c2021040124.lua

--- script/c2021040124.lua
+++ script/c2021040124.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	Duel.Hint(HINT_SKILL_COVER,1,1994|(BackEntryID<<32))
+	Duel.Hint(HINT_SKILL_COVER,1,1994|(1<<32))
 	local e1=Effect.CreateEffect(c) 
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
