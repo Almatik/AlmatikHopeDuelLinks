4c229d31a0fa5140adab2b03504523720bc5d35a
Almatik <almatiktk@gmail.com>
2021-06-17 21:09:25 +0600

    Update c2021040124.lua

--- script/c2021040124.lua
+++ script/c2021040124.lua
@@ -1,6 +1,7 @@
 --Extra Extra
 local s,id=GetID()
 function s.initial_effect(c)
+	Duel.Hint(HINT_SKILL_COVER,c:GetControler(),1994|(1994<<32))
 	--Activate
 	local e1=Effect.CreateEffect(c) 
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
@@ -34,10 +35,8 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.GetTurnPlayer()==tp
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	--place this card to the field
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-	Duel.Hint(HINT_SKILL_COVER,c:GetControler(),1994|(1994<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	local code=eg:GetFirst():GetCode()
 	local tc=Duel.CreateToken(tp,code)
