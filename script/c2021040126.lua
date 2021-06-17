18af53c030da097cf2608c1427a24bdd5bc8f44b
Almatik <almatiktk@gmail.com>
2021-06-18 00:31:24 +0600

    Fix

--- script/c2021040126.lua
+++ script/c2021040126.lua
@@ -6,10 +6,14 @@ function s.initial_effect(c)
 	aux.DuelLinksSkill(c,1994,EVENT_FREE_CHAIN,s.flipcon,s.flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--twice per duel check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
 	return aux.DLCanIgnition(tp) and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,3,nil,TYPE_TRAP)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--Used skill flag register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
 	--place this card to the field
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
