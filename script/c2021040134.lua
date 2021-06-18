80c268584349903b4e70b22d9d7da841646fb93a
Almatik <almatiktk@gmail.com>
2021-06-18 19:13:06 +0600

    Fix

---
+++ script/c2021040134.lua
@@ -0,0 +1,30 @@
+--Bandit 1
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksIgnition(c,2021040100,s.flipcon,s.flipop,1)
+end
+function s.filter(c)
+	return c:IsAbleToDeck()
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--twice per duel check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+		and Duel.IsPlayerCanDraw(tp,1)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Used skill flag register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
+		Duel.ShuffleDeck(tp)
+		
+	end
+end
