8211264de8c1a9a7a0c1f580b4772d18a6e415dd
Almatik <almatiktk@gmail.com>
2021-06-18 19:31:14 +0600

    Fix

---
+++ script/c2021040137.lua
@@ -0,0 +1,27 @@
+--Cyber Style 1
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksIgnition(c,2021040100,s.flipcon,s.flipop,1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--twice per duel check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+		and Duel.GetLP(tp)<=3000
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Used skill flag register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	--Field Spell
+	local c=e:GetHandler()
+	local code=Duel.CreateToken(tp,10080320)
+	Duel.MoveToField(code,tp,tp,LOCATION_MZONE,POS_FACEUP,true)
+	Duel.MoveToField(code,tp,tp,LOCATION_MZONE,POS_FACEUP,true)
+	Duel.MoveToField(code,tp,tp,LOCATION_MZONE,POS_FACEUP,true)
+end
