15add6a2b1a3ad719b915232f7171ab0be6e3a9a
Almatik <almatiktk@gmail.com>
2021-06-18 18:49:47 +0600

    Fix

--- script/c2021040130.lua
+++ script/c2021040130.lua
@@ -1,4 +1,4 @@
---Sealed Tombs 1
+--Sealed Tombs 3
 Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
@@ -8,6 +8,7 @@ end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
 	return aux.DLCanIgnition(tp) and Duel.GetFlagEffect(tp,id)==0
+		and Duel.GetLP(tp)<=Duel.GetLP(1-tp)+1000
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -25,16 +26,25 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTarget(s.sumlimit)
 	e1:SetReset(RESET_PHASE+PHASE_END,2)
 	Duel.RegisterEffect(e1,tp)
-	--flip during End phase
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e2:SetCountLimit(1)
-	e2:SetCondition(s.EPcon)
-	e2:SetOperation(s.EPop)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_REMOVE+EFFECT_FLAG_CLIENT_HINT)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetTarget(s.remlimit)
 	e2:SetReset(RESET_PHASE+PHASE_END,2)
 	Duel.RegisterEffect(e2,tp)
+	--flip during End phase
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.EPcon)
+	e3:SetOperation(s.EPop)
+	e3:SetReset(RESET_PHASE+PHASE_END,2)
+	Duel.RegisterEffect(e3,tp)
 end
 function s.sumlimit(e,c,sump,sumtype,sumpos,targetp)
 	return c:IsLocation(LOCATION_GRAVE)
