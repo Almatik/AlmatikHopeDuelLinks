68793be59c09ce2fb96c53ac371a6bc748321c7a
Almatik <almatiktk@gmail.com>
2021-06-18 10:43:43 +0600

    Fix

--- script/duellinks.lua
+++ script/duellinks.lua
@@ -37,17 +37,6 @@ end
 function Auxiliary.DLSkillOp(coverid,setcode,skillcon,skillop,countlimit)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
-		if skillop~=nil then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			e1:SetCode(setcode)
-			if type(countlimit)=="number" then
-				e1:SetCountLimit(countlimit)
-			end
-			e1:SetCondition(skillcon)
-			e1:SetOperation(skillop)
-			Duel.RegisterEffect(e1,e:GetHandlerPlayer())
-		end
 		Duel.DisableShuffleCheck(true)
 		Duel.SendtoDeck(c,tp,-2,REASON_RULE)
 		--generate the skill in the "skill zone"
@@ -68,17 +57,4 @@ function Auxiliary.DLSkillOp(coverid,setcode,skillcon,skillop,countlimit)
 	    c:RegisterEffect(e2)
 		e:Reset()
 	end
-end
-
--- Proc on Startup with custom operation
-
-function Auxiliary.DLStartUp(c,coverid,skillcon,skillop)
-	--activate
-	local e1=Effect.CreateEffect(c) 
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_STARTUP)
-	e1:SetRange(0x5f)
-	e1:SetOperation(Auxiliary.DLSkillOp(coverid,nil,nil,nil,nil))
-	c:RegisterEffect(e1)
 end
