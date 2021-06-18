0089bfc92bfb5e1a86d5b02444e45c2e7751e245
Almatik <almatiktk@gmail.com>
2021-06-18 12:08:41 +0600

    Update duellinks.lua

--- script/duellinks.lua
+++ script/duellinks.lua
@@ -16,55 +16,49 @@ end
 -- Proc for basic skill
 -- c: the card (card)
 -- coverNum: the Number of the cover (int)
--- setcode: the 
 -- skillcon: condition to activate the skill (function)
 -- skillop: operation related to the skill activation (function)
 -- countlimit: number of times you can use this skill
-function Auxiliary.DuelLinksSkill(c,coverid,setcode,skillcon,skillop,countlimit,skilltype)
-	if setcode==nil then local setcode=EVENT_FREE_CHAIN end
+-- skilltype: the type of the skill
+-- setcode: the EVENT code
+function Auxiliary.DuelLinksSkill(c,coverid,skillcon,skillop,countlimit,skilltype,setcode)
 	if coverid==nil then local coverid=c:GetCode() end
 	if skilltype==nil then local skilltype=SKILL_IGNITION end
+	if skilltype==SKILL_STARTUP then
+		local setcode=EVENT_STARTUP
+		local e1=Effect.CreateEffect(c) 
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_STARTUP)
+		e1:SetRange(0x5f)
+		e1:SetOperation(Auxiliary.DLSkillOp(coverid,skillcon,skillop,countlimit,setcode))
+		c:RegisterEffect(e1)
+	end
+	if skilltype==SKILL_PREDRAW then
+		local setcode=EVENT_PREDRAW
+		local e1=Effect.CreateEffect(c) 
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_STARTUP)
+		e1:SetRange(0x5f)
+		e1:SetOperation(Auxiliary.DLSkillOp(coverid,skillcon,skillop,countlimit,setcode))
+		c:RegisterEffect(e1)
+	end
 	if skilltype==SKILL_IGNITION then
+		local skillcode=EVENT_FREE_CHAIN
 		--activate
 		local e1=Effect.CreateEffect(c) 
 		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_STARTUP)
 		e1:SetRange(0x5f)
-		e1:SetOperation(Auxiliary.DLSkillOp(coverid,setcode,skillcon,skillop,countlimit))
-		c:RegisterEffect(e1)
-	end
-	if skilltype==SKILL_STARTUP then
-		local e1=Effect.CreateEffect(c) 
-		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_STARTUP)
-		e1:SetRange(0x5f)
-		e1:SetOperation(Auxiliary.DLStartUp(coverid))
-		c:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e2:SetCode(EVENT_STARTUP)
-		e2:SetCountLimit(countlimit)
-		e2:SetRange(0x5f)
-		e2:SetCondition(skillcon)
-		e2:SetOperation(skillop)
-		c:RegisterEffect(e2)
-	end
-	if skilltype==SKILL_PREDRAW then
-		local e1=Effect.CreateEffect(c) 
-		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_STARTUP)
-		e1:SetRange(0x5f)
-		e1:SetOperation(Auxiliary.DLSkillOp(coverid,EVENT_PREDRAW,skillcon,skillop,countlimit))
+		e1:SetOperation(Auxiliary.DLSkillOp(coverid,skillcon,skillop,countlimit,setcode))
 		c:RegisterEffect(e1)
 	end
 end
 
 -- Skill Ignition
-function Auxiliary.DLSkillOp(coverid,setcode,skillcon,skillop,countlimit)
+function Auxiliary.DLSkillOp(coverid,skillcon,skillop,countlimit,setcode)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
 		if skillop~=nil then
@@ -78,22 +72,6 @@ function Auxiliary.DLSkillOp(coverid,setcode,skillcon,skillop,countlimit)
 			e1:SetOperation(skillop)
 			Duel.RegisterEffect(e1,e:GetHandlerPlayer())
 		end
-		Duel.DisableShuffleCheck(true)
-		Duel.SendtoDeck(c,tp,-2,REASON_RULE)
-		--generate the skill in the "skill zone"
-		Duel.Hint(HINT_SKILL_COVER,c:GetControler(),coverid)
-		Duel.Hint(HINT_SKILL,c:GetControler(),c:GetCode())
-		--send to limbo then draw 1 if the skill was in the hand
-		if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
-			Duel.Draw(p,1,REASON_RULE)
-		end
-	end
-end
-
--- Skill StartUp
-function Auxiliary.DLStartUp(coverid)
-	return function(e,tp,eg,ep,ev,re,r,rp)
-		local c=e:GetHandler()
 		Duel.DisableShuffleCheck(true)
 		Duel.SendtoDeck(c,tp,-2,REASON_RULE)
 		--generate the skill in the "skill zone"
