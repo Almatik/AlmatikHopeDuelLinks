f610210c106f874ef021bf57d8e5218ed853383c
Almatik <almatiktk@gmail.com>
2021-06-18 10:55:02 +0600

    Update c2021040105.lua

--- script/c2021040105.lua
+++ script/c2021040105.lua
@@ -3,7 +3,16 @@ Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.DuelLinksStartUp(c,2021040100,flipcon,flipop)
+	aux.DuelLinksStartUp(c,2021040100,nil,nil,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetCondition(s.flipcon)
+	e1:SetOperation(s.flipop)
+	c:RegisterEffect(e1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
