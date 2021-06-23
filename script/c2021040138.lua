e70cb33be724a5dedd73f1bce41e3bb91572fbcf
Almatik <almatiktk@gmail.com>
2021-06-23 12:44:40 +0600

    Add files via upload

---
+++ script/c2021040138.lua
@@ -0,0 +1,46 @@
+--Titan Showdown
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksStartUp(c,2021040100,s.flipcon,s.flipop,1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return true
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Change Damage
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetCondition(s.con1)
+	e1:SetValue(s.value)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CHANGE_DAMAGE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(0,1)
+	e2:SetCondition(s.con2)
+	e2:SetValue(s.value)
+	c:RegisterEffect(e2)
+end
+function s.con1(e)
+	return Duel.GetLP(tp)>Duel.GetLP(1-tp)
+end
+function s.con1(e)
+	return Duel.GetLP(tp)<Duel.GetLP(1-tp)
+end
+function s.value1(e,re,dam,r,rp,rc)
+	if (r&REASON_BATTLE)~=0 then
+		return dam*2
+	else
+		return dam
+	end
+end
