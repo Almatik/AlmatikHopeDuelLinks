8634df12887e7ad9ffa3601c8aee46c223c2a7e6
Almatik <almatiktk@gmail.com>
2021-06-23 12:55:00 +0600

    Add files via upload

--- script/c2021040138.lua
+++ script/c2021040138.lua
@@ -3,33 +3,27 @@ Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.DuelLinksStartUp(c,2021040100,s.flipcon,s.flipop,1)
-end
-function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
-	return true
-end
-function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-	Duel.Hint(HINT_CARD,tp,id)
-	--Change Damage
-	local c=e:GetHandler()
+	aux.DuelLinksStartUp(c,2021040100,nil,s.flipop,1)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetCondition(s.con1)
 	e1:SetValue(s.value)
-	Duel.RegisterEffect(e1,tp)
+	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EFFECT_CHANGE_DAMAGE)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetTargetRange(0,1)
 	e2:SetCondition(s.con2)
 	e2:SetValue(s.value)
-	Duel.RegisterEffect(e2,tp)
+	c:RegisterEffect(e2)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
 end
 function s.con1(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetLP(tp)>Duel.GetLP(1-tp)
