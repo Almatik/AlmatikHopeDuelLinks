c11f90f0ea5455a8c6ea2d6bfa9c2dc776daac73
Almatik <almatiktk@gmail.com>
2021-06-23 11:15:13 +0600

    Add files via upload

--- script/c2021040136.lua
+++ script/c2021040136.lua
@@ -1,4 +1,4 @@
---Cyber Style 2
+--Cyber Style 1
 Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
@@ -16,21 +16,16 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--Used skill flag register
-	Duel.RegisterFlagEffect(ep,id,0,0,0)
 	--Field Spell
 	local c=e:GetHandler()
-	local n=4000-Duel.GetLP(tp)
-	if Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-		and n>=1000 then
-		Duel.MoveToField(Duel.CreateToken(tp,26439287),tp,tp,LOCATION_MZONE,POS_FACEUP,true)
-	end
-	if Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-		and n>=2000 then
-		Duel.MoveToField(Duel.CreateToken(tp,26439287),tp,tp,LOCATION_MZONE,POS_FACEUP,true)
-	end
-	if Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-		and n>=3000 then
-		Duel.MoveToField(Duel.CreateToken(tp,26439287),tp,tp,LOCATION_MZONE,POS_FACEUP,true)
-	end
+	local n=math.floor((4000-Duel.GetLP(tp))/1000)
+	repeat
+		Duel.RegisterFlagEffect(ep,id,0,0,0)
+		if Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+			then
+			local tc=Duel.CreateToken(tp,26439287)
+			Duel.MoveToField(tc,tp,tp,LOCATION_MZONE,POS_FACEUP,true)
+			
+		end
+	until Duel.GetFlagEffect(ep,id)==n
 end
