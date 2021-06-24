d91db0dedd97cfcf22ac6ccaf2c3e672e68a392c
Almatik <almatiktk@gmail.com>
2021-06-24 16:41:30 +0600

    Add files via upload

--- script/c2021040147.lua
+++ script/c2021040147.lua
@@ -5,18 +5,12 @@ function s.initial_effect(c)
 	--Activate
 	aux.DuelLinksIgnition(c,2021040100,s.flipcon,s.flipop,1)
 end
-	--Return filter
 function s.tgfilter(c,tp)
 	return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK+ATTRIBUTE_LIGHT) and c:IsAbleToDeck()
-		and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil,c:GetAttribute())
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,c:GetAttribute())
 end
-	--Filter to send 1 monster with opposite attribute of discard monster
-function s.filter1(c,att)
+function s.thfilter(c,att)
 	return c:IsAbleToHand() and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK+ATTRIBUTE_LIGHT) and not c:IsAttribute(att)
-end
-	--TO Hand filter
-function s.filter2(c)
-	return c:IsAttribute(ATTRIBUTE_DARK+ATTRIBUTE_LIGHT) and c:IsAbleToHand()
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--twice per duel check
@@ -24,7 +18,6 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
 	return aux.CanActivateSkill(tp)
 		and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_HAND,0,1,nil,tp)
-		and  Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
@@ -36,6 +29,6 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,tgfilter,tp,LOCATION_HAND,0,1,1,nil)
 	local att=g:GetFirst():GetAttribute()
 	Duel.SendtoDeck(g,nil,1,REASON_EFFECT)
-	local tc=Duel.SelectMatchingCard(tp,filter1,tp,LOCATION_DECK,0,1,1,nil,att)
+	local tc=Duel.SelectMatchingCard(tp,thfilter,tp,LOCATION_DECK,0,1,1,nil,att)
 	Duel.SendtoHand(tc,tp,REASON_RULE)
 end
