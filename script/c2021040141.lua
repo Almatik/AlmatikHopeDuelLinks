5788c6a3dec2408daccb1fafcfb917a564f7531c
Almatik <almatiktk@gmail.com>
2021-06-24 15:04:26 +0600

    Add files via upload

---
+++ script/c2021040141.lua
@@ -0,0 +1,19 @@
+--Chain Reaction
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksTrigger(c,2021040100,s.flipcon,s.flipop,nil,EVENT_CHAIN_SOLVED)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_TRAP) and rp==tp
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Field Spell
+	Duel.Damage(1-tp,200,REASON_EFFECT)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+end
