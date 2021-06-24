4c11d37851436b91388132c0271c69655f71ddba
Almatik <almatiktk@gmail.com>
2021-06-24 16:53:20 +0600

    Add files via upload

--- script/c2021040147.lua
+++ script/c2021040147.lua
@@ -5,25 +5,21 @@ function s.initial_effect(c)
 	--Activate
 	aux.DuelLinksIgnition(c,2021040100,s.flipcon,s.flipop,1)
 end
-function s.filter1(c,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK+ATTRIBUTE_LIGHT) and c:IsAbleToDeck()
-		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,c:GetAttribute())
+	--Discard filter
+function s.tgfilter(c,tp)
+	return c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK+ATTRIBUTE_LIGHT) and c:IsAbleToGraveAsCost()
+		and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil,c:GetAttribute())
 end
-function s.filter2(c,att)
-	return c:IsAbleToHand() and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK+ATTRIBUTE_LIGHT) and not c:IsAttribute(att)
-end
-function s.filterl(c)
-	return c:IsAbleToHand() and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_LIGHT)
-end
-function s.filterd(c)
-	return c:IsAbleToHand() and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK)
+	--Filter to send 1 monster with opposite attribute of discard monster
+function s.filter1(c,att)
+	return c:IsAbleToGrave() and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_DARK+ATTRIBUTE_LIGHT) and not c:IsAttribute(att)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--twice per duel check
 	if Duel.GetFlagEffect(ep,id)>1 then return end
 	--condition
 	return aux.CanActivateSkill(tp)
-		and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND,0,1,nil,tp)
+		and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_HAND,0,1,nil,tp
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
@@ -32,12 +28,9 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--Used skill flag register
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
 	local c=e:GetHandler()
-	local g=Duel.SelectMatchingCard(tp,filter1,tp,LOCATION_HAND,0,1,1,nil,tp)
+	local g=Duel.SelectMatchingCard(tp,tgfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
 	Duel.SendtoDeck(g,nil,1,REASON_EFFECT)
-	if g:GetFirst():IsAttribute(ATTRIBUTE_DARK) then
-		local tc=Duel.SelectMatchingCard(tp,filterl,tp,LOCATION_DECK,0,1,1,nil)
-	else
-		local tc=Duel.SelectMatchingCard(tp,filterd,tp,LOCATION_DECK,0,1,1,nil)
-	end
+	local att=Duel.GetOperatedGroup():GetFirst():GetAttribute()
+	local tc=Duel.SelectMatchingCard(tp,filter1,tp,LOCATION_DECK,0,1,1,nil,att)
 	Duel.SendtoHand(tc,tp,REASON_RULE)
 end
