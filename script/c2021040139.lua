f298de57cc87f8950fb19b07dc5efc5a80594120
Almatik <almatiktk@gmail.com>
2021-06-24 14:57:22 +0600

    Add files via upload

--- script/c2021040139.lua
+++ script/c2021040139.lua
@@ -1,4 +1,4 @@
---Last Gamble (2017)
+--Last Gamble (2019)
 Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
@@ -10,7 +10,7 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
 	return aux.CanActivateSkill(tp)
-		and Duel.GetTurnCount()>=5
+		and Duel.GetTurnCount()>=7
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
@@ -19,7 +19,9 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,tp,id)
 	--Field Spell
 	Duel.SetLP(tp,100)
-	Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD)
-	local dc=Duel.TossDice(tp,1)
-	Duel.Draw(tp,dc,REASON_EFFECT)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,2,2,nil)
+	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
+		local dc=Duel.TossDice(tp,1)
+		Duel.Draw(tp,dc,REASON_EFFECT)
+	end
 end
