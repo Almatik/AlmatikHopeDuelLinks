7fda9bc78688a4bc6444dacf70d169484fb97f9a
Almatik <almatiktk@gmail.com>
2021-06-24 16:28:48 +0600

    Add files via upload

--- script/c2021040147.lua
+++ script/c2021040147.lua
@@ -5,19 +5,30 @@ function s.initial_effect(c)
 	--Activate
 	aux.DuelLinksIgnition(c,2021040100,s.flipcon,s.flipop,1)
 end
-function s.filter1(c)
+function s.filter(c)
 	return c:IsAbleToDeck() and (c:IsAttribute(ATTRIBUTE_DARK) or c:IsAttribute(ATTRIBUTE_LIGHT))
-		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,c:GetAttribute())
 end
-function s.filter2(c,att)
-	return c:IsAbleToHand() and (c:IsAttribute(ATTRIBUTE_DARK) or c:IsAttribute(ATTRIBUTE_LIGHT)) and not c:IsAttribute(att)
+function s.filter1(c)
+	return c:IsAbleToDeck() and c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+function s.filter2(c)
+	return c:IsAbleToDeck() and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.filterl(c)
+	return c:IsAbleToHand() and c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+function s.filterd(c)
+	return c:IsAbleToHand() and c:IsAttribute(ATTRIBUTE_DARK)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--twice per duel check
 	if Duel.GetFlagEffect(ep,id)>1 then return end
 	--condition
 	return aux.CanActivateSkill(tp)
-		and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND,0,1,nil)
+		and (Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.filterd,tp,LOCATION_DECK,0,1,nil))
+		or (Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.filterl,tp,LOCATION_DECK,0,1,nil))
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
@@ -26,10 +37,13 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--Used skill flag register
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
 	local c=e:GetHandler()
-	local g=Duel.SelectMatchingCard(tp,filter1,tp,LOCATION_HAND,0,1,1,nil)
-	local att=g:GetFirst():GetAttribute()
-	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
-		local tc=Duel.SelectMatchingCard(tp,filter2,tp,LOCATION_DECK,0,1,1,nil,att)
+	local g=Duel.SelectMatchingCard(tp,filter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoDeck(g,nil,1,REASON_EFFECT)
+	if g:IsAttribute(ATTRIBUTE_LIGHT) then
+		local tc=Duel.SelectMatchingCard(tp,filterd,tp,LOCATION_DECK,0,1,1,nil)
+		Duel.SendtoHand(tc,tp,REASON_RULE)
+	else
+		local tc=Duel.SelectMatchingCard(tp,filterl,tp,LOCATION_DECK,0,1,1,nil)
 		Duel.SendtoHand(tc,tp,REASON_RULE)
 	end
 end
