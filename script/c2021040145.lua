803e405abaa363c6b6dc5b9bf664cbe6c9e18ed4
Almatik <almatiktk@gmail.com>
2021-06-24 15:45:42 +0600

    Add files via upload

---
+++ script/c2021040145.lua
@@ -0,0 +1,19 @@
+--Middle Age Mechs
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksStartUp(c,2021040100,s.flipcon,s.flipop,1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Field Spell
+	local c=e:GetHandler()
+	local code=Duel.CreateToken(tp,92001300)
+	Duel.MoveToField(code,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+end
