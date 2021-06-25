20a7246144a45e95b798986ecfa266ab807318c1
Almatik <almatiktk@gmail.com>
2021-06-25 13:33:53 +0600

    Add files via upload

---
+++ script/c2021040149.lua
@@ -0,0 +1,51 @@
+--Masked Tribute (2019)
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksIgnition(c,2021040100,s.flipcon,s.flipop,1)
+end
+function s.filter(c)
+	return c:IsAbleToDeck()
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--twice per duel check
+	if Duel.GetFlagEffect(ep,id)>1 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+		and Duel.GetCustomActivityCount(id,tp,ACTIVITY_SUMMON)==0
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Cannot Special Summon monsters this turn, except Dragon monsters
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.sumlimit)
+	e1:SetReset(RESET_PHASE+PHASE_END,2)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	Duel.RegisterEffect(e2,tp)
+	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,1),nil)
+	--Used skill flag register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
+		local ac=Duel.GetRandomNumber(1,#s.mask)
+		local code=s.mask[ac]
+		local tc=Duel.CreateToken(tp,code)
+		Duel.MoveToField(tc,tp,tp,LOCATION_MZONE,POS_FACEUP,true)
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+		s[2+tp]=0
+	end
+end
+function s.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsCode(48948935)
+end
+s.mask = {13676474,86569121}
