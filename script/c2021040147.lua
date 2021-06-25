212d48e8afe0731bd777c69ef640588a5842f005
Almatik <almatiktk@gmail.com>
2021-06-25 10:12:25 +0600

    Add files via upload

--- script/c2021040147.lua
+++ script/c2021040147.lua
@@ -8,7 +8,7 @@ end
 function s.filter(c,att)
 	return c:IsAbleToHand() and c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and not c:IsAttribute(att)
 end
-function s.tgtfilter(c,e,tp)
+function s.tgtfilter(c,tp)
 	return c:IsAbleToDeck() and c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK)
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,c:GetAttribute())
 end
@@ -17,7 +17,7 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(ep,id)>1 then return end
 	--condition
 	return aux.CanActivateSkill(tp)
-		and Duel.IsExistingTarget(s.tgtfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+		and Duel.IsExistingTarget(s.tgtfilter,tp,LOCATION_HAND,0,1,nil,tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
@@ -26,8 +26,8 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--Used skill flag register
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
 	local c=e:GetHandler()
-	local tc=Duel.SelectTarget(tp,s.tgtfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local tc=Duel.SelectTarget(tp,s.tgtfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
 	Duel.SendtoDeck(tc,nil,1,REASON_EFFECT)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,c:GetAttribute())
-	Duel.SendtoHand(tc,tp,REASON_RULE)
+	Duel.SendtoHand(g,tp,REASON_RULE)
 end
