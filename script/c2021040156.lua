ffdbe028fde2ddb0f053e07426d04da28576c323
Almatik <almatiktk@gmail.com>
2021-06-28 14:30:47 +0600

    Add files via upload

---
+++ script/c2021040156.lua
@@ -0,0 +1,45 @@
+--Sorcery Conduit (2019)
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksPredraw(c,s.flipcon,s.flipop,1)
+	aux.GlobalCheck(s,function()
+		s[0]=nil
+		s[1]=nil
+		s[2]=0
+		s[3]=0
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if not s[tp] then s[tp]=Duel.GetLP(tp) end
+	if s[tp]>Duel.GetLP(tp) then
+		s[2+tp]=s[2+tp]+(s[tp]-Duel.GetLP(tp))
+		s[tp]=Duel.GetLP(tp)
+	end
+end
+function s.filter(c)
+	return c:IsRace(RACE_SPELLCASTER)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and tp==Duel.GetTurnPlayer()
+		and Duel.GetDrawCount(tp)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil)
+		and s[2+tp]>=1000
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--ask if you want to activate the skill or not
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	local g=Duel.GetFirstMatchingCard(s.filter,tp,LOCATION_DECK,0,nil)
+	if Duel.MoveSequence(g,0)~=0 then
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+		s[2+tp]=0
+	end
+end
