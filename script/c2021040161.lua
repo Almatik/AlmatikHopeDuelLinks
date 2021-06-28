40a03d4ebcb0b5d0701affc526189418b3c01637
Almatik <almatiktk@gmail.com>
2021-06-28 16:51:11 +0600

    Add files via upload

---
+++ script/c2021040161.lua
@@ -0,0 +1,19 @@
+--Master of Rites
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksStartUp(c,s.flipcon,s.flipop,1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Field Spell
+	local c=e:GetHandler()
+	local code=Duel.CreateToken(tp,25796442)
+	Duel.MoveToField(code,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+end
