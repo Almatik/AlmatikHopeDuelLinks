378e2b5a3757899e1b0da13ecb1fe85422740ca9
Almatik <almatiktk@gmail.com>
2021-06-29 13:54:45 +0600

    Add files via upload

---
+++ script/c2021040175.lua
@@ -0,0 +1,42 @@
+--Baggy Sleeves
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksPredraw(c,s.flipcon,s.flipop,1)
+	local e1=Effect.CreateEffect(c) 
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetOperation(s.checkop)
+	c:RegisterEffect(e1)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if eg:GetPreviousControler()==tp then
+		Duel.RegisterFlagEffect(tp,id,0,0,0)
+	end
+end
+function s.filter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and tp==Duel.GetTurnPlayer()
+		and Duel.GetDrawCount(tp)==1 and Duel.GetFlagEffect(tp,id)>0
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--ask if you want to activate the skill or not
+	if Duel.GetFlagEffect(tp,id+1)>0 and not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
+	Duel.RegisterFlagEffect(tp,id+1,0,0,0)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Draw
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_DRAW_COUNT)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(2)
+	e1:SetReset(RESET_PHASE+PHASE_DRAW)
+	Duel.RegisterEffect(e1,tp)
+end
