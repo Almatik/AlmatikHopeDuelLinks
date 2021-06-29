382b646859264dfb67fb770f5a70557ce7b5fa84
Almatik <almatiktk@gmail.com>
2021-06-29 09:46:30 +0600

    Add files via upload

---
+++ script/c2021040164.lua
@@ -0,0 +1,49 @@
+--Bring It
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksIgnition(c,s.flipcon,s.flipop,1)
+	aux.GlobalCheck(s,function()
+		s[0]=nil
+		s[1]=nil
+		s[2]=0
+		s[3]=0
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if not s[tp] then s[tp]=Duel.GetLP(tp) end
+	if s[tp]>Duel.GetLP(tp) then
+		s[2+tp]=s[2+tp]+(s[tp]-Duel.GetLP(tp))
+		s[tp]=Duel.GetLP(tp)
+	end
+end
+function s.filter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_WARRIOR) and c:IsLevel(8)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--twice per duel check
+	if Duel.GetFlagEffect(ep,id)>1 then return end
+	--condition
+	return aux.CanActivateSkill(tp) and s[2+tp]>=1000
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Used skill flag register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
+		local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil)
+		Duel.SendtoHand(tc,tp,REASON_RULE)
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+		s[2+tp]=0
+	end
+end
