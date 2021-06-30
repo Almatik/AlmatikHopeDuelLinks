8d7eba17577131fdc4eae1e6181edb290828df41
Almatik <almatiktk@gmail.com>
2021-06-30 08:28:56 +0600

    Add files via upload

---
+++ script/c2021040177.lua
@@ -0,0 +1,24 @@
+--Shadow Game
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksTrigger(c,s.flipcon,s.flipop,nil,EVENT_PHASE)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentPhase()==PHASE_END
+end
+function s.filter(c)
+	return c:IsType(TYPE_MONSTER)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Field Spell
+	local p=Duel.GetTurnPlayer()
+	local d=Duel.GetMatchingGroupCount(s.filter,p,LOCATION_GRAVE,0,nil)*100
+	Duel.Damage(p,d,REASON_RULE)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+end
