5f2e4958423e31f3b99aa194a081b9f48ecd74c9
Almatik <almatiktk@gmail.com>
2021-06-30 23:22:32 +0600

    f

--- script/c2021040117.lua
+++ script/c2021040117.lua
@@ -3,22 +3,35 @@ Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.DuelLinksTrigger(c,s.flipcon,s.flipop,1,EVENT_PREDRAW)
+	aux.DuelLinksStartUp(c,s.flipcon,s.flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
-	return Duel.GetCurrentChain()==0
-		and Duel.GetTurnPlayer()==tp
-		and (Duel.GetTurnCount()==1
-		or Duel.GetTurnCount()==2)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	--look at hand
-	Duel.Draw(tp,1,REASON_EFFECT)
-	Duel.Draw(1-tp,1,REASON_EFFECT)
+	Duel.Draw(tp,1,REASON_RULE)
+	Duel.Draw(1-tp,1,REASON_RULE)
+	local e1=Effect.CreateEffect(c) 
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PREDRAW)
+	e1:SetRange(0x5f)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+		and (Duel.GetTurnCount()==1
+		and Duel.GetTurnCount()==2)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -35,5 +48,5 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetTargetRange(1,0)
 	e2:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e2,tp)
-	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,2),nil)
+	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,2),nil)  
 end
--- script/c2021040118.lua
+++ script/c2021040118.lua
@@ -3,14 +3,11 @@ Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.DuelLinksTrigger(c,s.flipcon,s.flipop,1,EVENT_PREDRAW)
+	aux.DuelLinksStartUp(c,s.flipcon,s.flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
-	return Duel.GetCurrentChain()==0
-		and Duel.GetTurnPlayer()==tp
-		and (Duel.GetTurnCount()==1
-		or Duel.GetTurnCount()==2)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -19,12 +16,29 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--look at hand
 	Duel.Draw(tp,1,REASON_RULE)
 	Duel.Draw(1-tp,1,REASON_RULE)
+	local e1=Effect.CreateEffect(c) 
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PREDRAW)
+	e1:SetRange(0x5f)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+		and (Duel.GetTurnCount()==1
+		and Duel.GetTurnCount()==2)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetTargetRange(1,0)
+	e1:SetValue(1)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,1),nil)
+	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,1),nil)	
 end
