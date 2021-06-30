6a6463a344632ec56c48857dab5fede8f5935f99
Almatik <almatiktk@gmail.com>
2021-06-30 23:05:36 +0600

    f

--- script/c2021040115.lua
+++ script/c2021040115.lua
@@ -11,20 +11,23 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.filter(c)
+	return c:IsFaceup()
+end
+function s.filter5(c)
 	return c:IsLevelAbove(5)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
-	local atk=Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_MZONE,0,nil)*300
-	if #g==0 or atk==0 then return end
+	local g=Duel.GetMatchingGroup(s.filter5,tp,LOCATION_MZONE,0,nil)
+	local atk=Duel.GetMatchingGroupCount(s.filter5,tp,LOCATION_MZONE,0,nil)
+	if #g==0 then return end
 	for tc in aux.Next(g) do
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(atk*300)
 		tc:RegisterEffect(e1)
 	end
 	--flip during End phase
