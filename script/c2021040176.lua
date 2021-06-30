4adaa5ed0957dea9f899feec0c6aa5b04da0a08c
Almatik <almatiktk@gmail.com>
2021-06-30 08:15:09 +0600

    Add files via upload

--- script/c2021040176.lua
+++ script/c2021040176.lua
@@ -5,14 +5,14 @@ function s.initial_effect(c)
 	--Activate
 	aux.DuelLinksIgnition(c,s.flipcon,s.flipop,1)
 end
-function s.filter(c)
+function s.filter(c,tp)
 	return c:IsControler(tp) and c:IsPreviousControler(tp)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--twice per duel check
 	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
-	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil,tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
