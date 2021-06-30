322bb7576f937e317691a3edd556a36eeebe7d94
Almatik <almatiktk@gmail.com>
2021-06-30 08:13:46 +0600

    Add files via upload

---
+++ script/c2021040176.lua
@@ -0,0 +1,29 @@
+--See You Later
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksIgnition(c,s.flipcon,s.flipop,1)
+end
+function s.filter(c)
+	return c:IsControler(tp) and c:IsPreviousControler(tp)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--twice per duel check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Used skill flag register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SendtoHand(g,tp,REASON_RULE)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+	s[2+tp]=0
+end
+
