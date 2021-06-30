1d7bf9c85c53336066afa058b725d0e3c3da29b3
Almatik <almatiktk@gmail.com>
2021-06-30 08:35:56 +0600

    Add files via upload

--- script/c2021040177.lua
+++ script/c2021040177.lua
@@ -3,11 +3,7 @@ Duel.LoadScript("duellinks.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.DuelLinksTrigger(c,s.flipcon,s.flipop,nil,EVENT_PHASE)
-end
-function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
-	return Duel.GetCurrentPhase()==PHASE_END
+	aux.DuelLinksTrigger(c,true,s.flipop,nil,EVENT_PHASE+PHASE_END)
 end
 function s.filter(c)
 	return c:IsType(TYPE_MONSTER)
