326bb2062f6e2a7b9da4df28b4820444c79b18d2
Almatik <almatiktk@gmail.com>
2021-06-30 19:26:29 +0600

    f

--- script/c2021040102.lua
+++ script/c2021040102.lua
@@ -4,7 +4,17 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	aux.DuelLinksIgnition(c,s.flipcon,s.flipop,1)
-	aux.DLAdjustLP(c)
+	aux.GlobalCheck(s,function()
+		s[0]=nil
+		s[1]=nil
+		s[2]=0
+		s[3]=0
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
 end
 function s.filter(c)
 	return c:IsAbleToDeck()
--- script/duellinks.lua
+++ script/duellinks.lua
@@ -3,36 +3,6 @@ HINT_SKILL_COVER = 201
 HINT_SKILL_FLIP  = 202
 HINT_SKILL_REMOVE = 203
 --function that return if the player (tp) can activate the skill
-function Auxiliary.DLAdjustLP(c)
-	local s=GetID(c)
-	aux.GlobalCheck(s,function()
-		s[0]=nil
-		s[1]=nil
-		s[2]=0
-		s[3]=0
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_ADJUST)
-		ge1:SetOperation(Auxiliary.AdjustLP)
-		Duel.RegisterEffect(ge1,0)
-	end)
-end
-function Auxiliary.AdjustLP(e)
-	local tp=e:GetHandlerPlayer()
-	if not s[tp] then s[tp]=Duel.GetLP(tp) end
-	if s[tp]>Duel.GetLP(tp) then
-		s[2+tp]=s[2+tp]+(s[tp]-Duel.GetLP(tp))
-		s[tp]=Duel.GetLP(tp)
-	end
-end
-function Auxiliary.CheckLP(e,num)
-	local tp=e:GetHandlerPlayer()
-	return s[2+tp]>=num
-end
-function Auxiliary.ResetLP()
-	local tp=e:GetHandlerPlayer()
-	s[2+tp]=0
-end
 
 -- Proc for basic skill
 -- c: the card (card)
