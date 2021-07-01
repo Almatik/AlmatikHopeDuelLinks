37727daa7b169b400948aa6a513ca8ce747e3bf2
Almatik <almatiktk@gmail.com>
2021-07-01 19:47:24 +0600

    f

---
+++ script/c2021040182.lua
@@ -0,0 +1,71 @@
+--Grit (2018)
+function c2021040182.initial_effect(c)
+	aux.DuelLinksTrigger(c,s.checkcon,s.checkop,1,EVENT_PREDRAW)
+	local e1=Effect.CreateEffect(c) 
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(PRE_DAMAGE_CALCULATE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	e1:SetCondition(s.flipcon1)
+	e1:SetOperation(s.flipop1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c) 
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	e2:SetCondition(s.flipcon2)
+	e2:SetOperation(s.flipcon2)
+	c:RegisterEffect(e2)
+end
+function s.checkcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetLP(tp)>=4000
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	--random
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,0)
+end
+function s.flipcon1(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetFlagEffect(tp,id)==1
+		and Duel.GetBattleDamage(tp)>=Duel.GetLP(tp)
+end
+function s.flipop1(e,tp,eg,ep,ev,re,r,rp)
+	--random
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	Duel.ChangeBattleDamage(tp,Duel.GetLP(tp)-1)
+end
+function s.flipcon2(e,tp,eg,ev,ep,re,r,rp)
+	local e1=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_DAMAGE)
+	local e2=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
+	local rd=e1 and not e2
+	local rr=not e1 and e2
+	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
+	if ex and (cp==tp or cp==PLAYER_ALL) and not rd 
+		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and Duel.GetLP(tp)<=cv then 
+		return true 
+	end
+	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
+	return ex and (cp==tp or cp==PLAYER_ALL) and rr 
+		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and Duel.GetLP(tp)<=cv
+end
+function s.flipop2(e,tp,eg,ev,ep,re,r,rp)
+	local c=e:GetHandler()
+	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetLabel(cid)
+	e1:SetValue(s.damval)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.damval(e,re,val,r,rp,rc)
+	local cc=Duel.GetCurrentChain()
+	if cc==0 or (r&REASON_EFFECT)==0 then return val end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	if cid~=e:GetLabel() then return val end
+	return Duel.GetLP(tp)-1
+end
