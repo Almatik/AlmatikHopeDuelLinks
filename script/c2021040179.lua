a5b245d9c039a4a5f5d1bdc1824d12836aac17ac
Almatik <almatiktk@gmail.com>
2021-07-01 18:57:43 +0600

    f

---
+++ script/c2021040179.lua
@@ -0,0 +1,42 @@
+--Level Augmentation
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksIgnition(c,s.flipcon,s.flipop,1)
+end
+function s.cfilter(c,tp)
+	return c:IsLevelAbove(1) and not c:IsPublic()
+		and Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.lvfilter(c)
+	return c:IsLevelAbove(1) and c:IsFaceup()
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--twice per duel check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,tp)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--Used skill flag register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
+	Duel.ConfirmCards(1-tp,cg)
+	Duel.ShuffleHand(tp)
+	local lv=cg:GetFirst():GetLevel()
+	local tc=Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
