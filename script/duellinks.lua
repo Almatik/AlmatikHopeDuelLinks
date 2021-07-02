e1812cd57f9ad660070d4d1a76a12e888157f29d
Almatik <almatiktk@gmail.com>
2021-07-02 23:10:03 +0600

    Update duellinks.lua

--- script/duellinks.lua
+++ script/duellinks.lua
@@ -92,9 +92,6 @@ function Auxiliary.DLSkillOp(skillcon,skillop,countlimit,setcode)
 			tc:Cover(coverid)
 		end
 		Duel.ShuffleDeck(tp)
-		local g=Duel.GetFieldGroup(tp,LOCATION_EXTRA,0)
-		local tc=g:GetFirst()
-		Duel.MoveSequence(tc,1)
 		--send to limbo then draw 1 if the skill was in the hand
 		if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
 			Duel.Draw(p,1,REASON_RULE)
