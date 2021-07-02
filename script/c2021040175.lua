b7c949a911ddb341a6fb29dd7bb0901484029746
Almatik <almatiktk@gmail.com>
2021-07-02 21:05:10 +0600

    Update c2021040175.lua

--- script/c2021040175.lua
+++ script/c2021040175.lua
@@ -4,43 +4,29 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	aux.DuelLinksPredraw(c,s.flipcon,s.flipop,1)
-	local e1=Effect.CreateEffect(c) 
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_DESTROYED)
-	e1:SetCondition(s.checkcon)
-	e1:SetOperation(s.checkop)
-	c:RegisterEffect(e1)
 end
-function s.checkfilter(c,tp)
-	return c:IsReason(REASON_BATTLE) and c:IsPreviousControler(tp) and c:IsLevelAbove(1)
-end
-function s.checkcon(e,tp,eg,ep,ev,re,r,rp)
-   return eg:IsExists(s.checkfilter,1,nil,tp) 
-end
-function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(tp,id,0,0,0)
-	Duel.Damage(tp,100,REASON_RULE)
-end
-function s.filter(c)
-	return c:IsAttribute(ATTRIBUTE_DARK)
+function s.filter(c,e,tp,tid)
+	return c:GetReason()&0x21==0x21 and c:GetTurnID()==tid-1
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	local tid=Duel.GetTurnCount()
 	--condition
 	return Duel.GetCurrentChain()==0 and tp==Duel.GetTurnPlayer()
-		and Duel.GetDrawCount(tp)==1 and Duel.GetFlagEffect(tp,id)>0
+		and Duel.GetDrawCount(tp)==1 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,tid)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--ask if you want to activate the skill or not
-	if Duel.SelectYesNo(tp,aux.Stringid(id,0))==0 then return end
-	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-	Duel.Hint(HINT_CARD,tp,id)
-	--Draw
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_DRAW_COUNT)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetValue(2)
-	e1:SetReset(RESET_PHASE+PHASE_DRAW)
-	Duel.RegisterEffect(e1,tp)
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+		Duel.Hint(HINT_CARD,tp,id)
+		--Draw
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_DRAW_COUNT)
+		e1:SetProperty(EFFECT_FLAG_PL   AYER_TARGET)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(2)
+		e1:SetReset(RESET_PHASE+PHASE_DRAW)
+		Duel.RegisterEffect(e1,tp)
+	end
 end
