4135971239a92a25b857d16a4f0c4e40f2ffbd6b
Almatik <almatiktk@gmail.com>
2021-07-02 20:46:41 +0600

    Update c2021040177.lua

--- script/c2021040177.lua
+++ script/c2021040177.lua
@@ -8,10 +8,7 @@ end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetTurnPlayer()
 	--condition
-	return Duel.IsExistingMatchingCard(s.filter,p,LOCATION_GRAVE,0,1,nil)
-end
-function s.filter(c)
-	return c:IsType(TYPE_MONSTER)
+	return Duel.GetFieldGroup(p,LOCATION_GRAVE,0):GetCount()>0
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
@@ -19,7 +16,7 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,tp,id)
 	--Field Spell
 	local p=Duel.GetTurnPlayer()
-	local d=Duel.GetMatchingGroupCount(s.filter,p,LOCATION_GRAVE,0,nil)*100
+	local d=Duel.GetFieldGroup(p,LOCATION_GRAVE,0):GetCount()*100
 	Duel.Damage(p,d,REASON_RULE)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
 end
