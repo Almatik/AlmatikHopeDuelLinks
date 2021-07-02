268a633306a96e28d756ed6d6074cda09f754b7e
Almatik <almatiktk@gmail.com>
2021-07-02 22:10:03 +0600

    Update c2021040182.lua

--- script/c2021040182.lua
+++ script/c2021040182.lua
@@ -20,7 +20,7 @@ function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.flipcon1(e,tp,eg,ep,ev,re,r,rp)
 	--condition
-	return Duel.GetBattleDamage(tp)>=Duel.GetLP(tp)
+	return not Duel.GetBattleDamage(tp)<Duel.GetLP(tp)
 end
 function s.flipop1(e,tp,eg,ep,ev,re,r,rp)
 	--random
@@ -34,40 +34,5 @@ function s.flipop1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e1,tp)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ChangeBattleDamage(tp,1)
-end
-
-function s.flipcon2(e,tp,eg,ev,ep,re,r,rp)
-	local e1=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_DAMAGE)
-	local e2=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
-	local rd=e1 and not e2
-	local rr=not e1 and e2
-	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
-	if ex and (cp==tp or cp==PLAYER_ALL) and not rd 
-		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and Duel.GetLP(tp)<=cv then 
-		return true 
-	end
-	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
-	return ex and (cp==tp or cp==PLAYER_ALL) and rr 
-		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and Duel.GetLP(tp)<=cv
-end
-function s.flipop2(e,tp,eg,ev,ep,re,r,rp)
-	local c=e:GetHandler()
-	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetLabel(cid)
-	e1:SetValue(s.damval)
-	e1:SetReset(RESET_CHAIN)
-	Duel.RegisterEffect(e1,tp)
-end
-function s.damval(e,re,val,r,rp,rc)
-	local cc=Duel.GetCurrentChain()
-	if cc==0 or (r&REASON_EFFECT)==0 then return val end
-	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
-	if cid~=e:GetLabel() then return val end
-	return Duel.GetLP(tp)-1
+	Duel.ChangeBattleDamage(tp,Duel.GetLP(tp)-1)
 end
