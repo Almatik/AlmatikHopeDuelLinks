0a96be9dffe2cf8c1f2915327778523cb2f0d865
Almatik <almatiktk@gmail.com>
2021-07-02 20:06:28 +0600

    а

--- script/c2021040153.lua
+++ script/c2021040153.lua
@@ -13,7 +13,7 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
 	return aux.CanActivateSkill(tp)
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--place this card to the field
